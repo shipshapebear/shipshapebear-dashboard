3f1975c63c9df635881c90437e43ff59e7466af5
Aaron Jay Malabanan <malabanan.aaronjay@gmail.com>
2023-07-29 16:08:23 +0800

    fixed sidebar tooltip

--- components/side-bar.tsx
+++ components/side-bar.tsx
@@ -14,7 +14,6 @@ const Sidebar = () => {
     const pathname = usePathname()
 
     return (
-
         <div className={cn("sticky left-0 top-0 z-20 h-full min-h-[100vh] border-r border-border bg-background", toggled ? "[&~main]:ml-[var(--aside-width)]" : " [&~main]:ml-[var(--collapsed-aside-width)]")}>
             <div className={cn("relative h-full transition-all duration-200 ease-in-out",
                 toggled ? "w-[300px]"
@@ -26,45 +25,37 @@ const Sidebar = () => {
                         className={cn("!hover:bg-none flex gap-3")}>
                         <Image alt='logo' src={SiteLogo} className='mx-auto block h-auto w-[30px]' /> {toggled && <span className='font-bold'>shipshapebear</span>}
                     </Link>
-                    <button onClick={() => setToggled(!toggled)} className={cn("absolute right-[-9px] h-[20px] w-[20px]  rounded-full bg-accent outline-1")}>
+                    <button id="collapsed-button" type='button' onClick={() => setToggled(!toggled)} className={cn("absolute right-[-10px] h-[20px] w-[20px]  rounded-full bg-accent outline-1")}>
                         <IoIosArrowBack className={cn("m-auto text-foreground", !toggled ? "rotate-180" : "rotate-[0]")} />
                         <span className='sr-only'>collapse sidebar</span>
                     </button>
                 </div>
-
-                <ul className='p-3'>
-                    {[
-                        { value: "Dashboard", link: "/dashboard" },
-                        { value: "About", link: "/about" },
-                        { value: "Services", link: "/services" },
-                        { value: "Payment", link: "/payment" }
-                    ].map((val, index) => (
-                        <>
-                            {!toggled ?
-                                <TooltipProvider disableHoverableContent delayDuration={300} key={val.value}>
-                                    <Tooltip>
-                                        <TooltipTrigger asChild>
-                                            <li className={`cursor-pointer rounded-sm  hover:bg-accent hover:text-accent-foreground ${pathname === val.link ? "bg-accent" : ""}`}>
-                                                <Link href={val.link} className='flex items-center'>
-                                                    <CiLink className={cn("mx-4 h-10 w-6", toggled ? "mx-4 inline-block " : "mx-auto block")} />
-                                                    {toggled && <span>{val.value}</span>}
-                                                </Link>
-                                            </li>
-                                        </TooltipTrigger>
-                                        <TooltipContent side='right'>
-                                            {val.value}
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider> :
-                                <li key={val.value + index} className={`cursor-pointer rounded-sm  hover:bg-accent hover:text-accent-foreground ${pathname === val.link ? "bg-accent" : ""}`}>
-                                    <Link href={val.link} className='flex items-center'>
-                                        <CiLink className={cn("mx-4 h-10 w-6", toggled ? "mx-4 inline-block " : "mx-auto block")} />
-                                        {toggled && <span>{val.value}</span>}
-                                    </Link>
-                                </li>}
-                        </>
-                    ))}
-                </ul>
+                <TooltipProvider disableHoverableContent delayDuration={300} >
+                    <ul className='p-3'>
+                        {[
+                            { value: "Dashboard", link: "/dashboard" },
+                            { value: "About", link: "/about" },
+                            { value: "Services", link: "/services" },
+                            { value: "Payment", link: "/payment" }
+                        ].map((val) => (
+                            <li key={val.value}>
+                                <Tooltip>
+                                    <TooltipTrigger asChild>
+                                        <div className={`cursor-pointer rounded-sm  hover:bg-accent hover:text-accent-foreground ${pathname === val.link ? "bg-accent" : ""}`}>
+                                            <Link href={val.link} className='flex items-center'>
+                                                <CiLink className={cn("mx-4 h-10 w-6", toggled ? "mx-4 inline-block " : "mx-auto block")} />
+                                                {toggled && <span>{val.value}</span>}
+                                            </Link>
+                                        </div>
+                                    </TooltipTrigger>
+                                    {!toggled && <TooltipContent side='right'>
+                                        {val.value}
+                                    </TooltipContent>}
+                                </Tooltip>
+                            </li>
+                        ))}
+                    </ul>
+                </TooltipProvider>
             </div>
         </div>
     )
