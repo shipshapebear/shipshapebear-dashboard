b74f90acf062d7bef121c0aeb409340da1ed29af
shipshapebear <shipshapebear@gmail.com>
2023-08-04 12:48:55 +0800

    added drizzle orm

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { profile } from "@/drizzle/schema"
+import { db } from "@/drizzle/connection"
 
-const Page = () => {
+export const revalidate = 0
+const getData = async () => {
+  const allUsers = await db.select().from(profile);
+
+  if (!allUsers) return null
+
+  return allUsers
+
+}
+
+const Page = async () => {
+  const data = await getData()
   return (
-    <div>Page</div>
+    <pre>{JSON.stringify(data, null, 2)}</pre>
   )
 }
 
