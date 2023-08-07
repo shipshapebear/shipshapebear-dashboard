d9bd45479caacf0da7e24ad9f4d63d071181269a
shipshapebear <shipshapebear@gmail.com>
2023-08-08 00:49:55 +0800

    fix issue where drizzle config does not get the database url from env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,15 @@
 import type { Config } from "drizzle-kit";
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+  path: '.env.local',
+});
  
 export default {
   schema: "./drizzle/schema.ts",
   out: "./drizzle",
   driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL as string,
   }
 } satisfies Config;
