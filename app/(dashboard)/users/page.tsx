5168b259b1035d9c142f8b4cde8ad4c0583a4893
shipshapebear <shipshapebear@gmail.com>
2023-08-11 14:35:56 +0800

    optimize users query

--- app/(dashboard)/users/page.tsx
+++ app/(dashboard)/users/page.tsx
@@ -1,44 +1,19 @@
 import { notFound } from "next/navigation"
 import { columns } from "./columns"
 import { DataTable } from "./data-table"
-import { createClient } from '@supabase/supabase-js'
 import { profile } from "@/drizzle/schema"
 import { db } from "@/drizzle/connection"
 
 export const revalidate = 0
-async function getData() {
-    // Fetch data from your API here.
-    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_ADMIN_SERVICE_ROLE_KEY, {
-        auth: {
-            autoRefreshToken: false,
-            persistSession: false
-        }
-    })
 
-    // Access auth admin api
-    const adminAuthClient = supabase.auth.admin
-    const { data: { users }, error } = await adminAuthClient.listUsers()
+async function getData() {
     const userProfiles = await db.select().from(profile);
 
-    const profileMap: any = {};
-    userProfiles?.forEach((profileData) => {
-        profileMap[profileData.id] = profileData;
-    });
-
-    // Merge the data
-    const mergedData = users.map((userData) => {
-        const { id } = userData;
-        return {
-            ...userData,
-            ...profileMap[id] || null,
-        };
-    });
-
-    if (!profile || !users) {
+    if (!profile) {
         notFound()
     }
 
-    return mergedData
+    return userProfiles
 }
 
 export default async function Page() {
@@ -46,7 +21,7 @@ export default async function Page() {
 
     return (
         <div className="container mx-auto py-10">
-            <h1 className='mb-4 text-2xl font-bold text-foreground capitalize'>{process.env.NODE_ENV} Users</h1>
+            <h1 className='mb-4 text-2xl font-bold capitalize text-foreground'>{process.env.NODE_ENV} Users</h1>
             <DataTable columns={columns} data={data} />
         </div>
     )
--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, pgEnum, pgSchema, AnyPgColumn, uuid, text, timestamp, bigint } from "drizzle-orm/pg-core"
+import { pgTable, pgEnum, pgSchema, AnyPgColumn, bigint, text, timestamp, uuid } from "drizzle-orm/pg-core"
 
 export const keyStatus = pgEnum("key_status", ['default', 'valid', 'invalid', 'expired'])
 export const keyType = pgEnum("key_type", ['aead-ietf', 'aead-det', 'hmacsha512', 'hmacsha256', 'auth', 'shorthash', 'generichash', 'kdf', 'secretbox', 'secretstream', 'stream_xchacha20'])
@@ -10,6 +10,15 @@ export const requestStatus = pgEnum("request_status", ['PENDING', 'SUCCESS', 'ER
 
 import { sql } from "drizzle-orm"
 
+export const products = pgTable("products", {
+	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
+	id: bigint("id", { mode: "number" }).primaryKey().notNull(),
+	title: text("title"),
+	description: text("description"),
+	price: text("price"),
+	createdAt: timestamp("created_at", { withTimezone: true, mode: 'string' }).defaultNow(),
+});
+
 export const profile = pgTable("profile", {
 	id: uuid("id").primaryKey().notNull(),
 	role: text("role").default('admin'),
@@ -18,13 +27,5 @@ export const profile = pgTable("profile", {
 	username: text("username"),
 	website: text("website"),
 	displayName: text("display_name"),
-});
-
-export const products = pgTable("products", {
-	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
-	id: bigint("id", { mode: "number" }).primaryKey().notNull(),
-	title: text("title"),
-	description: text("description"),
-	price: text("price"),
-	createdAt: timestamp("created_at", { withTimezone: true, mode: 'string' }).defaultNow(),
+	email: text("email"),
 });
